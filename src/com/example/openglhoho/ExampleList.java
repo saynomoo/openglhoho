54158ffe115331850ab9028d77a6c9a7ba2bff4f
Tuomas Huhtanen <tuomas.huhtanen@reaktor.fi>
2014-02-03 00:41:04 +0200

    Add icon for the particle system

--- src/com/example/openglhoho/ExampleList.java
+++ src/com/example/openglhoho/ExampleList.java
@@ -55,7 +55,7 @@ class Example {
         return new ArrayList<Example>() {{
             add(new Example(R.drawable.cube, "Cube", "Draw indexed vertexes. Draw texture. Use colors. Rotate by touch events."));
             add(new Example(R.drawable.donut, "Donut", "Create triangle strip mesh."));
-            add(new Example(0, "Particles", "A texture based star shower"));
+            add(new Example(R.drawable.stars, "Particles", "A texture based star shower"));
         }};
     }
 }
