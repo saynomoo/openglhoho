70146026683f08c5f5676c1a081a3a79d3d17407
Tuomas Huhtanen <tuomas.huhtanen@reaktor.fi>
2014-02-02 15:55:49 +0200

    Try to avoid of flipping triangles to inside out by removing the first degenerate triangle

--- src/com/example/openglhoho/Donut.java
+++ src/com/example/openglhoho/Donut.java
@@ -36,7 +36,7 @@ public class Donut {
     private byte[] createIndexes(int x, int y) {
         ArrayList<Byte> al = new ArrayList<Byte>();
         for(int i=0; i<x-1; i++) {
-            al.add((byte) (i*y));
+            if(i>0)al.add((byte) (i*y));
             for(int j=0; j<y; j++) {
                 al.add((byte) (i*y+j));
                 al.add((byte) ((i+1)*y+j));
